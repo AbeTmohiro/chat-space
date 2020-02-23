772bf513fb06b0dc6706bf64d274e837b4593a28
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-02-23 16:59:36 +0900

    Stashed changes

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,26 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+<<<<<<< Updated upstream
   module ChatSpace
     class Application < Rails::Application
       config.generators do |g|
+=======
+module ChatSpace
+  class Application < Rails::Application
+    config.generators do |g|
+>>>>>>> Stashed changes
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
+<<<<<<< Updated upstream
       end
       config.i18n.default_locale = :ja
     end
   end
+=======
+    end
+  end
+end
+>>>>>>> Stashed changes
