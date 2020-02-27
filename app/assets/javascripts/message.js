952e5f4abe4e980d01d584d6c567c165bda4d909
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-02-27 11:49:24 +0900

    Organize indents
    インデント調整

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -42,28 +42,28 @@ $(function(){
  }
  
 $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.chat-main__messages').append(html);
-      $('form')[0].reset();
-      $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
-    })
-    .always(function(){
-      $('.submit-btn').prop('disabled',false)
-    })
-    .fail(function() {
-      alert("メッセージ送信に失敗しました");
-    });
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.chat-main__messages').append(html);
+    $('form')[0].reset();
+    $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+  })
+  .always(function(){
+    $('.submit-btn').prop('disabled',false)
+  })
+  .fail(function() {
+    alert("メッセージ送信に失敗しました");
+  });
 })
 });
